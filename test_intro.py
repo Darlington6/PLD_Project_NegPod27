843d5164fbffc5c710271e30f08a0e0e25b5d590
makito042 <makuochiokoye@gmail.com>
2024-04-14 11:09:39 -0700

    2nd test update

--- test_intro.py
+++ test_intro.py
@@ -10,3 +10,12 @@ class TestDiseaseIdentification(unittest.TestCase):
     def test_symptoms(self):
         self.assertIsInstance(symptoms, list)
         self.assertGreater(len(symptoms), 0)
+
+    def test_get_input(self):
+        prompt = "Welcome to Dr. Chat! We help you identify possible diseases you might be suffering from based on the symptoms you are having.\n\nTo get started, please choose the symptoms you have from the list below:\n1. dry cough\n2. fever\n3. runny nose\n4. fatigue\n5. sore throat\n6. muscle aches\n7. cough\n8. shortness of breath\n9. mild fever\n10. loss of taste or smell\n11. sneezing\n12. lack of appetite\n13. headache\n14. vomiting\n15. thirst\n16. diarrhea\n17. irritability\n18. low blood pressure\n19. stomach ache\n20. weakness"
+        expected_input = {symptoms[0], symptoms[1], symptoms[2]}
+        actual_input = get_input(prompt)
+        self.assertIsInstance(actual_input, set)
+        self.assertEqual(actual_input, expected_input)
+        self.assertIsInstance(list(actual_input)[0], str)
+        # self.assertTrue(all(symptom.isdigit() for symptom in actual_input))
