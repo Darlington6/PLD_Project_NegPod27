b277e528edd7d4fefbcb5cc9548547ec071b8545
Darlington6 <d.tunyinko@alustudent.com>
2024-04-14 15:13:49 +0200

    Added an import function

--- data_extraction.py
+++ data_extraction.py
@@ -1,26 +1,5 @@
-for disease in diseases:
-    link = disease.get('href')
-    disease_page = requests.get(link)
-    disease_soup = BeautifulSoup(disease_page.content, 'html.parser')
-    whole_page = disease_soup.textimport requests
+import requests
 from bs4 import BeautifulSoup
 from openai import OpenAI
 from app.db.models import Disease, Symptom
 from app.db import get_db, init_db
-
-
-init_db()
-db_gen = get_db()
-db = next(db_gen)
-
-client = OpenAI(
-    api_key='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'
-)
-
-origin = 'https://www.nhsinform.scot/illnesses-and-conditions/a-to-z/'
-page = requests.get(origin)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-diseases = soup.select('.az_list_indivisual > ul > li > a')
-
