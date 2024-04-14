6587c843ae5697c1f8b811cec3c42c87b9e55ff9
Darlington6 <d.tunyinko@alustudent.com>
2024-04-14 15:15:39 +0200

    Initiallizing

--- data_extraction.py
+++ data_extraction.py
@@ -3,3 +3,8 @@ from bs4 import BeautifulSoup
 from openai import OpenAI
 from app.db.models import Disease, Symptom
 from app.db import get_db, init_db
+
+
+init_db()
+db_gen = get_db()
+db = next(db_gen)
